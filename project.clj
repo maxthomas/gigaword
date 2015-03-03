e460542cf56a1b09b266831b2026e1b87ebb9dd6
Max Thomas <maxjthomas@gmail.com>
2015-03-03 13:18:57 -0500

    Version 2.0.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject gigaword "2.0.0-SNAPSHOT"
+(defproject gigaword "2.0.0"
   :description "Gigaword Clojure/Java API."
   :url "https://github.com/maxthomas/gigaword"
   :license {:name "Eclipse Public License"
