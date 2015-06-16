24cbc0566555594955b7bbfa92ed0d01e8952dbe
Max Thomas <max.thomas@jhu.edu>
2015-06-16 18:41:19 -0400

    Version 3.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject gigaword "3.1.0-SNAPSHOT"
+(defproject gigaword "3.1.0"
   :description "Gigaword Clojure API."
   :url "https://github.com/maxthomas/gigaword"
   :license {:name "Eclipse Public License"
